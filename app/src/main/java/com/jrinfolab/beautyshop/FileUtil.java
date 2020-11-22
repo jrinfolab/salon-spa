5a6232437d7850a13c70f2339a8a675749d913a7
maruthupandiyan <jrinfolab@gmail.com>
2020-11-22 14:42:00 +0530

    - inclusion of ImageSlide completed - Dummy map image added

---
+++ app/src/main/java/com/jrinfolab/beautyshop/FileUtil.java
@@ -0,0 +1,46 @@
+package com.jrinfolab.beautyshop;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class FileUtil {
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    public static File createTempImageFile(Context context) {
+        File image = null;
+        try {
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmssSSS").format(new Date());
+            String imageFileName = "JPEG_" + timeStamp + "_";
+            File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            image = File.createTempFile(imageFileName, ".jpeg", storageDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    public static String[] getStringUriList(List<Uri> uriList) {
+        String[] stringUriList = new String[uriList.size()];
+        for (int i = 0; i < uriList.size(); i++) {
+            stringUriList[i] = (uriList.get(i)).toString();
+        }
+        return stringUriList;
+    }
+}
